de83202ca23eb20e226cfbc39d3f2a5efb7c9850
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2025-07-19 06:32:28 +0000

    tested

--- Decision Control and Looping Statements/2eveOdd.sh
+++ Decision Control and Looping Statements/2eveOdd.sh
@@ -1,6 +1,5 @@
 #write a program to find whether the given number is odd or even
 
 num=$1
-
 str=("Even" "odd")
 echo ${str[($num & 1)]}
--- Decision Control and Looping Statements/3leapYear.sh
+++ Decision Control and Looping Statements/3leapYear.sh
@@ -1,8 +1,6 @@
 #write a program to find whether a given year is a leap year or not
 
 year=$1
-
 str=("a leap year." "not a leap year.")
 (( leap_year = (( (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) ) ? 0 : 1) ))
-
 echo "$year is ${str[leap_year]}"
