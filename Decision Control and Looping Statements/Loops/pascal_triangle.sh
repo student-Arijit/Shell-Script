ee29ddffae4cbf9b93c465b5b8f93281f9ec941d
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2025-07-22 05:57:53 +0000

    pascal's triangle

---
+++ Decision Control and Looping Statements/Loops/pascal_triangle.sh
@@ -0,0 +1,16 @@
+row=5
+coef=1
+for ((i=0; i<row; i++)); do 
+    for ((space=0; space<row-i; space++)); do
+        printf " "
+    done
+    for ((j=0; j<=i; j++));do 
+        if [ $j -eq 0 ] || [ $i -eq 0 ]; then
+            coef=1
+        else
+            coef=$((coef * (i - j + 1) / j))
+        fi
+        printf "%2d" $coef
+    done 
+    printf "\n"
+done
